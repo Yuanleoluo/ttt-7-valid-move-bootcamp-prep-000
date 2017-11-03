07ce6c43722adc8b4d026cd47ec8f6a1f23d0988
Yuanleoluo <leoyuanluo@gmail.com>
2017-11-03 21:32:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,19 @@
 # code your #valid_move? method here
 def valid_move?(b, i)
-
+  if position_taken?(b, i)
+  else
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(a, b)
+  if a[b] === " "
+    false
+  elsif a[b] === ""
+    false
+  elsif a[b] === nil
+    false
+  elsif a[b] === "O" || a[b] === "X"
+    true
+  end
+end
